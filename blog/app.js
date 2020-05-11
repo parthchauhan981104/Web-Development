ac7052cd24361dbf89f75f7c030dd84b5d59a9c5
parthchauhan981104 <48384607+parthchauhan981104@users.noreply.github.com>
2020-05-11 14:34:44 +0530

    added mongoDB database using mongoose

--- blog/app.js
+++ blog/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,31 +13,61 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-let posts = [];
+const postSchema = {
+
+ title: String,
+ content: String
+
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res){
 
-  res.render ("home", {homecont: homeStartingContent, allposts: posts});
+  Post.find({}, function(err, posts){
+    console.log(posts)
+    if (posts.length !== 0){
+      res.render("home", {
+
+        homecont: homeStartingContent,
+        allposts: posts
+
+        });
+    } else{
+      res.render("home", {
+
+        homecont: homeStartingContent,
+        allposts: [{title: "No posts yet...", content: ""}]
+
+        });
+    }
+
+
+
+ })
 
 });
 
-app.get("/posts/:title", function(req, res){
-  var requestedTitle = _.lowerCase(req.params.title);
-  found_posts = [];
+app.get("/posts/:posttitle", function(req, res){
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-    if (requestedTitle === storedTitle){
-      found_posts.push(post);
-    }
-  });
+  var requestedPosttitle = _.toLower(req.params.posttitle);
+  console.log(requestedPosttitle)
 
-  res.render ("post", {found_posts: found_posts});
-  console.log(req.params);
+  Post.findOne({title: requestedPosttitle}, function(err, post){
+     console.log(post)
+     if (post!=null){
+       res.render("post", {fpost: post});
+     } else{
+       res.render("post", {fpost: {title: "No posts found", content: ""}});
+     }
+
+
+   });
+
 });
 
 app.get("/about", function(req, res){
@@ -58,10 +89,20 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-   const postdet = {title: req.body.title, content: req.body.newpost};
-   posts.push(postdet);
-   console.log(posts);
-   res.redirect("compose");
+
+  const post = new Post ({
+   title: req.body.title,
+   content: req.body.newpost
+  });
+
+  post.save(function(err){
+
+   if (!err){
+     res.redirect("compose");
+   }
+
+  });
+
 });
 
 
